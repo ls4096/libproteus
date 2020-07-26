ec80f81669ae18eb57319a38c9f3af871bf6e2ed
ls4096 <ls4096@8bitbyte.ca>
2020-07-26 18:08:42 -0400

    Add license.

--- include/proteus/ScalarConv.h
+++ include/proteus/ScalarConv.h
@@ -1,5 +1,17 @@
 /**
- * This file contains a list of some simple but useful scalar value conversion functions.
+ * Copyright (C) 2020 ls4096 <ls4096@8bitbyte.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef _proteus_ScalarConv_h_
@@ -9,6 +21,9 @@
 extern "C" {
 #endif // __cplusplus
 
+/**
+ * This file contains a list of some simple but useful scalar value conversion functions.
+ */
 
 // Converts metres to nautical miles.
 double proteus_ScalarConv_m2nm(double m);
--- lib/Compass.c
+++ lib/Compass.c
@@ -1,5 +1,20 @@
-#include "proteus/Compass.h"
+/**
+ * Copyright (C) 2020 ls4096 <ls4096@8bitbyte.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
+#include "proteus/Compass.h"
 
 double proteus_Compass_diff(double a, double b)
 {
